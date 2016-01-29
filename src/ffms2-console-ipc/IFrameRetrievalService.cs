d893a8d2dce190c9f396924ae83062a45ac6f599
Drew Peterson <dpeterson@roadrangerusa.com>
2016-01-29 12:00:05 -0600

    Add support for seek handling

--- src/ffms2-console-ipc/IFrameRetrievalService.cs
+++ src/ffms2-console-ipc/IFrameRetrievalService.cs
@@ -14,18 +14,32 @@ namespace ffms2.console.ipc
 
         Exception LastException { get; }
 
+        /// <summary>
+        /// Set the seek handling mode for new video sources
+        /// </summary>
+        /// <param name="mode">The seek handling mode to use</param>
+        /// <remarks>
+        /// As video sources are cached, you must set the seek handling mode before retrieving any frames
+        /// </remarks>
+        void SetSeekHandling(SeekHandling mode);
+
         void SetFrameOutputFormat(int width = 0, int height = 0,
             FrameResizeMethod frameResizeMethod = FrameResizeMethod.Bicubic,
             FramePixelFormat pixelFormat = FramePixelFormat.YV12);
 
-        IFrame GetFrame(int trackNumber, int frameNumber);
+        /// <summary>
+        /// Retrieve a List of all frames in the specified track
+        /// </summary>
+        /// <param name="trackNumber">The track to retrieve frames for</param>
+        /// <returns></returns>
+        List<IFrame> GetFrames(int trackNumber);
 
-        List<IFrame> GetFrameInfos(int trackNumber);
+        IFrame GetFrame(int trackNumber, int frameNumber);
 
         IFrame GetFrameAtPosition(int trackNumber, long position);
 
         IFrame GetFrameAtTime(int trackNumber, double time);
 
-        void DisplayFrame(IFrame frame, IntPtr windowId);
+        IFrame DisplayFrame(IFrame frame, IntPtr windowId);
     }
 }
---
+++ src/ffms2-console-ipc/SeekHandling.cs
@@ -0,0 +1,52 @@
+﻿namespace ffms2.console.ipc
+{
+    /// <summary>
+    /// Control how seeking is handled
+    /// </summary>
+    public enum SeekHandling
+    {
+        /// <summary>
+        /// Linear access without rewind
+        /// </summary>
+        /// <remarks>
+        /// <para>In FFMS2, the equivalent is <c>FFMS_SEEK_LINEAR_NO_RW</c>.</para>
+        /// <para>Will throw an error if each successive requested frame number isn't bigger than the last one.</para>
+        /// <para>Only intended for opening images but might work on well with some obscure video format.</para>
+        /// </remarks>
+        LinearNoRewind = -1,
+        /// <summary>
+        /// Linear access
+        /// </summary>
+        /// <remarks>
+        /// <para>In FFMS2, the equivalent is <c>FFMS_SEEK_LINEAR</c>.</para>
+        /// <para>If you request frame n without having requested frames 0 to n-1 in order first, all frames from 0 to n will have to be decoded before n can be delivered.</para>
+        /// <para>The definition of slow, but should make some formats "usable".</para>
+        /// </remarks>
+        Linear = 0,
+        /// <summary>
+        /// Safe normal
+        /// </summary>
+        /// <remarks>
+        /// <para>In FFMS2, the equivalent is <c>FFMS_SEEK_NORMAL</c>.</para>
+        /// <para>Bases seeking decisions on the keyframe positions reported by libavformat.</para>
+        /// </remarks>
+        Normal = 1,
+        /// <summary>
+        /// Unsafe normal
+        /// </summary>
+        /// <remarks>
+        /// <para>In FFMS2, the equivalent is <c>FFMS_SEEK_UNSAFE</c>.</para>
+        /// <para>Same as <see cref="Normal" /> but no error will be thrown if the exact destination has to be guessed.</para>
+        /// </remarks>
+        Unsafe = 2,
+        /// <summary>
+        /// Aggressive
+        /// </summary>
+        /// <remarks>
+        /// <para>In FFMS2, the equivalent is <c>FFMS_SEEK_AGGRESSIVE</c>.</para>
+        /// <para>Seeks in the forward direction even if no closer keyframe is known to exist.</para>
+        /// <para>Only useful for testing and containers where libavformat doesn't report keyframes properly.</para>
+        /// </remarks>
+        Aggressive = 3
+    }
+}
